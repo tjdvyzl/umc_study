0fa6e9549c5773770c8ead7edf11d40e965f18fe
tjdvyzl <tjdvyzl12@naver.com>
2023-11-05 09:05:09 +0900

    week 7 mission

--- src/pages/TV.jsx
+++ src/pages/TV.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import MovieCard from "./MovieCard";
+import { api_url, my_key } from "../config";
+import { MyMovieContainer } from "./MovieCard.style";
 
-export default function TV() {
+function TV() {
+  const [movieDatas, setMovieDatas] = useState([]);
+  useEffect(() => {
+    const endpoint = `${api_url}tv/popular?api_key=${my_key}&language=en-US&page=1`;
+
+    fetch(endpoint)
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        setMovieDatas(responseData.results);
+      });
+  }, []);
+
   return (
-    <div>TV</div>
-  )
+    <MyMovieContainer>
+      {movieDatas &&
+        movieDatas.map((movie, index) => {
+          return <MovieCard key={index} movieData={movie} />;
+        })}
+    </MyMovieContainer>
+  );
 }
+
+export default TV;
